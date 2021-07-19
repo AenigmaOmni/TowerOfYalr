d13c8fe93f0653c67e8bef0319b61e869b9ae24e
SolarOmni <johnathan.crocker@outlook.com>
2021-07-19 03:42:35 -0500

    Initial commit

---
+++ states/main/object_proxy_loader.go
@@ -0,0 +1,30 @@
+components {
+  id: "proxy_play"
+  component: "/states/main/proxy_play.collectionproxy"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script_proxy_loader"
+  component: "/states/main/script_proxy_loader.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
---
+++ states/play/object_tilemap_dungeon.go
@@ -0,0 +1,30 @@
+components {
+  id: "tilemap_dungeon"
+  component: "/states/play/tilemap_dungeon.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "script_generate_dungeon"
+  component: "/states/play/script_generate_dungeon.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
