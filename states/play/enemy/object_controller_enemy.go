0449e929bd6d194568fd346479b1e2dcc9dd0287
SolarOmni <johnathan.crocker@outlook.com>
2021-07-21 07:53:11 -0500

    Working on enemies

---
+++ states/play/enemy/object_controller_enemy.go
@@ -0,0 +1,30 @@
+components {
+  id: "script_controller_enemy"
+  component: "/states/play/script_controller_enemy.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "factory_enemy_spooder"
+  component: "/states/play/factory_enemy_spooder.factory"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- states/play/enemy/object_enemy_skeleton.go
+++ states/play/enemy/object_enemy_skeleton.go
@@ -1,30 +1,15 @@
-components {
-  id: "script_player_mover"
-  component: "/states/play/script_player_mover.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/res/graphics/main_atla.atlas\"\n"
-  "default_animation: \"hero\"\n"
+  "default_animation: \"skeleton\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
-    x: 8.0
-    y: -8.0
-    z: 3.0
+    x: 0.0
+    y: 0.0
+    z: 0.0
   }
   rotation {
     x: 0.0
